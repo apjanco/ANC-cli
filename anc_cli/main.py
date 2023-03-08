1f91cd68b5dd0979c193252397f9d9256273015a
apjanco <apjanco@gmail.com>
2023-03-08 12:10:50 -0500

    back to fuzzy matcher

--- anc_cli/main.py
+++ anc_cli/main.py
@@ -5,8 +5,16 @@ import srsly
 from anc_cli.utils import *
 import spacy
 from spacy.matcher import Matcher
+from spaczz.matcher import FuzzyMatcher
+
 import yaml
 from rich import print
+
+#TODO 
+#1. back to FuzzMatcher
+#2. walk subfolders, alta 
+#3. make rows 
+#4. run on all pdfs. 
 
 settings = yaml.load(Path('anc_cli/settings.yml').read_text(), Loader=yaml.Loader)
 api_key = yaml.load(Path('anc_cli/apikey.yml').read_text(), Loader=yaml.Loader)
@@ -19,15 +27,23 @@ municipio_names = srsly.read_json('anc_cli/data/municipios.json')
 departamentos = srsly.read_json('anc_cli/data/departamentos.json')
 
 
-place_matcher = Matcher(nlp.vocab)
+place_matcher = FuzzyMatcher(nlp.vocab)
 for municipio in municipio_names:
-    pattern = [{"LOWER": {"FUZZY": f"{m}"}} for m in nlp(municipio)]
-    place_matcher.add('municipio_'+municipio, [pattern])
+  place_matcher.add('municipio_'+municipio, [nlp(municipio)])
 for departamento in departamentos:
-    pattern = [{"LOWER": {"FUZZY": f"{m}"}} for m in nlp(departamento)]
-    place_matcher.add('departamento_'+departamento, [pattern])
+  place_matcher.add('departamento_'+departamento, [nlp(departamento)])
+
+#place_matcher = Matcher(nlp.vocab)
+# for municipio in municipio_names:
+#     pattern = [{"LOWER": {"FUZZY1": f"{m}"}} for m in nlp(municipio)]
+#     place_matcher.add('municipio_'+municipio, [pattern])
+# for departamento in departamentos:
+#     pattern = [{"LOWER": {"FUZZY1": f"{m}"}} for m in nlp(departamento)]
+#     place_matcher.add('departamento_'+departamento, [pattern])
 
 terms = settings['terms']
+print(terms)
+match_ratio = 85
 
 language = settings['language']
 output_dir = Path('anc_cli/output')
@@ -39,7 +55,7 @@ existing_data = [f.stem for f in output_dir.iterdir()]
 def process(pdf_directory:str, force: bool = typer.Option(False, "--force", help='Ignore existing data and create new.')):
     pdf_directory = Path(pdf_directory)
     if pdf_directory.exists():
-        print(f"[green] Processing {len(list(pdf_directory.iterdir()))} file [/green]")
+        print(f"[green] Processing {len(list(pdf_directory.iterdir()))} files [/green]")
 
         data = []
         for i, file_ in enumerate(pdf_directory.iterdir()):
@@ -57,27 +73,35 @@ def process(pdf_directory:str, force: bool = typer.Option(False, "--force", help
         output = []
         if data:
             for index, page in enumerate(data):
-                typer.echo(f"Processing page {index + 1}")
+                typer.echo(f"Processing page {index + 1} of {len(data)}")
                 for response in page['responses']:
                     for i, annotation in enumerate(response['textAnnotations']):
                         if i == 0:
                             # Process full text of document, identify pages that contain relevant terms
                             full_text = annotation.get('description', None)
                             doc = nlp(full_text)
-                            term_matcher = Matcher(nlp.vocab)
+                            # term_matcher = Matcher(nlp.vocab)
+                            # for term in terms:
+                            #     pattern = [{"LOWER": {"FUZZY1": f"{m}"}} for m in nlp(term)]
+                            term_matcher = FuzzyMatcher(nlp.vocab)
+                            #term_matcher.add(term, [pattern])
                             for term in terms:
-                                pattern = [{"LOWER": {"FUZZY": f"{m}"}} for m in nlp(term)]
-                                term_matcher.add(term, [pattern])
-
+                                term_matcher.add(term.upper(), [nlp(term)])
                             matches = term_matcher(doc)
-                            terms_found = [match_id for match_id, start, end in matches]
-                    
+                            #terms_found = [match_id for match_id, start, end in matches]
+                            terms_found = [match_id for match_id, start, end, ratio in matches if ratio > match_ratio]
+
                         if terms_found:
                             doc_w_term.append(i)
                             matches = place_matcher(doc)
-                            spans = [doc[start:end] for match_id, start, end in matches]
-                            match_ids = [match_id for match_id, start, end in matches]
+                            spans = [doc[start:end] for match_id, start, end, ratio in matches if ratio > match_ratio]
+                            match_ids = [match_id for match_id, start, end, ratio in matches if ratio > match_ratio]
                             doc_places[i] = [dict(index=i,start=span.start_char, end=span.end_char, text=span.text, match_id=match_id) for span, match_id in zip(spacy.util.filter_spans(spans), match_ids)]
+           
+                            # matches = place_matcher(doc)
+                            # spans = [doc[start:end] for match_id, start, end in matches]
+                            # match_ids = [match_id for match_id, start, end in matches]
+                            # doc_places[i] = [dict(index=i,start=span.start_char, end=span.end_char, text=span.text, match_id=match_id) for span, match_id in zip(spacy.util.filter_spans(spans), match_ids)]
                         
                     
             if doc_w_term: 
@@ -94,31 +118,49 @@ def process(pdf_directory:str, force: bool = typer.Option(False, "--force", help
                                 doc = nlp(f"{result['prior_word']} {result['value']} {result['next_word']}")
                                 span_matches = place_matcher(doc)
                                 for token_match, span_match in zip(token_matches, span_matches):
+                                    if token_match[3] > match_ratio or span_match[3] > match_ratio:
+                                        #ex. has span ('departamento_cauca', 0, 1, 100) ('departamento_valle del cauca', 0, 2, 75)
+                                        #ex. not span ('municipio_barranquilla', 0, 1, 100) ('departamento_atlántico', 0, 1, 89)
+                                        token_term = token_match[0].split("_")[0]
+                                        span_term = span_match[0].split("_")[0]
+                                        m = {}
+                                        if span_term == token_term:
+                                            m['match_term'] = span_term.title()
+                                            m['match_name'] = span_match[0].split("_")[1].title().replace('Del','del')
+                                        else:
+                                            m['match_term'] = token_term.title()
+                                            m['match_name'] = token_match[0].split("_")[1].title().replace('Del','del')
+                                        m['filename'] = result["filename"]
+                                        m['page'] = result["page"]
+                                        output.append(m)
+                                # for token_match, span_match in zip(token_matches, span_matches):
                                     
-                                    #ex. has span ('departamento_cauca', 0, 1, 100) ('departamento_valle del cauca', 0, 2, 75)
-                                    #ex. not span ('municipio_barranquilla', 0, 1, 100) ('departamento_atlántico', 0, 1, 89)
-                                    string_id = nlp.vocab.strings[token_match[0]]
-                                    token_term = string_id.split("_")[0]
+                                #     #ex. has span ('departamento_cauca', 0, 1, 100) ('departamento_valle del cauca', 0, 2, 75)
+                                #     #ex. not span ('municipio_barranquilla', 0, 1, 100) ('departamento_atlántico', 0, 1, 89)
+                                #     string_id = nlp.vocab.strings[token_match[0]]
+                                #     token_term = string_id.split("_")[0]
 
-                                    string_id = nlp.vocab.strings[span_match[0]]
-                                    span_term = string_id.split("_")[0]
-                                    m = {}
-                                    if span_term == token_term:
-                                        m['match_term'] = span_term.title()
-                                        string_id = nlp.vocab.strings[span_match[0]]
-                                        m['match_name'] = string_id.split("_")[1].title().replace('Del','del')
-                                    else:
-                                        m['match_term'] = token_term.title()
-                                        string_id = nlp.vocab.strings[token_match[0]]
-                                        m['match_name'] = string_id.split("_")[1].title().replace('Del','del')
-                                m['filename'] = result["filename"]
-                                m['page'] = result["page"]
-                                output.append(m)
+                                #     string_id = nlp.vocab.strings[span_match[0]]
+                                #     span_term = string_id.split("_")[0]
+                                #     m = {}
+                                #     if span_term == token_term:
+                                #         m['match_term'] = span_term.title()
+                                #         string_id = nlp.vocab.strings[span_match[0]]
+                                #         m['match_name'] = string_id.split("_")[1].title().replace('Del','del')
+                                #     else:
+                                #         m['match_term'] = token_term.title()
+                                #         string_id = nlp.vocab.strings[token_match[0]]
+                                #         m['match_name'] = string_id.split("_")[1].title().replace('Del','del')
+                                # m['filename'] = result["filename"]
+                                # m['page'] = result["page"]
+                                # output.append(m)
                                 
             if output:
+                # TODO before DataFrame create rows for pairs of Muni/Depart
                 df = pd.DataFrame(output)
                 df = df.drop_duplicates()
                 print(df)
+                df.to_csv('output.csv')
 
     else:
         typer.echo("Not a valid path, please check and try again.")
